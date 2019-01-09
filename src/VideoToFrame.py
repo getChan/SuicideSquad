24277a19bb89c0a38a9d6bea5f1aa62bd3945d88
getChan <9511chn@gmail.com>
2019-01-09 16:15:18 +0900

    Extract skin color

--- src/VideoToFrame.py
+++ src/VideoToFrame.py
@@ -1,24 +1,35 @@
 import cv2
+import numpy as np
 
-vidcap = cv2.VideoCapture('../data/raw/감사(1)_Trim.mp4')
-frameCount = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
-print('framecount', frameCount)
-framePer30 = frameCount // 30
-success, image = vidcap.read()
+def VidToFrame(load_path, save_path):
+    vidcap = cv2.VideoCapture(load_path)
+    frameCount = vidcap.get(cv2.CAP_PROP_FRAME_COUNT)
+    print('framecount', frameCount)
 
-if not success :
-    print('Video Read Error')
-count = 0
-while success:
-  success, image = vidcap.read()
-  if count % framePer30 == 0:
-      grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-      _, thresh = cv2.threshold(grayscale, 128, 255, cv2.THRESH_BINARY)
-    #   thresh = cv2.adaptiveThreshold(grayscale,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-    #   cv2.THRESH_BINARY,15,-2)
-    #   thresh = cv2.adaptiveThreshold(grayscale,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-    #   cv2.THRESH_BINARY,15,-2)
-      cv2.imwrite("../data/frames/thank%d.jpg" % count, thresh)     # save frame as JPEG file
-  if cv2.waitKey(10) == 27:                     # exit if Escape is hit
-      break
-  count += 1
+    success, image = vidcap.read()
+
+    if not success :
+        print('Video Read Error')
+    cnt = 0
+    while success:
+        success, image = vidcap.read()
+        
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+        lower_skin = np.array([0, 133, 77])
+        upper_skin = np.array([255, 173, 127])
+
+        mask = cv2.inRange(hsv, lower_skin, upper_skin)
+
+        cv2.imwrite(save_path%cnt, mask)     # save frame as JPEG file
+        if cv2.waitKey(10) == 27:                     # exit if Escape is hit
+            break
+        cnt += 1
+
+    vidcap.release()
+    pass
+
+
+if __name__ == '__main__':
+    load_path = '..\\data\\raw\\감사(1)_Trim.mp4'
+    save_path = "..\\data\\frames\\thank%d.jpg"
+    VidToFrame(load_path, save_path)
