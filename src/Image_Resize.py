3bd30acf3060e5e3e2340718f7f6e223ba637a4c
hyunseok4475 <33482120+hyunseok4475@users.noreply.github.com>
2019-01-22 15:05:12 +0900

    파일 경로까지 받아서 하는거

--- src/Image_Resize.py
+++ src/Image_Resize.py
@@ -1,14 +1,34 @@
+#%%
+#import modules
 import cv2
 import numpy as np
-
+import glob
+from os import listdir
+from os.path import isfile, join
+#%%
+#variable
 width = 112
 height = 112
 
-def resize(imgPath,svPath):
-    src = cv2.imread(imgPath, cv2.IMREAD_COLOR)
-    dst = cv2.resize(src, dsize=(width, height), interpolation=cv2.INTER_LINEAR)
-    cv2.imwrite(svPath, dst)
+#%%
+#function
+def resize(loadPath,savePath):
+        images = listdir(loadPath)
 
-loadpath = 'C:\\Users\\zmfna\\Desktop\\hand.jpg'
-savepath = 'C:\\Users\\zmfna\\Desktop\\hand1.jpg'
-resize(path)
+        print(images)
+        for i in range(len(images)):
+                image = images[i]
+                cnt = 0
+                while image:
+                        src = cv2.imread(image, cv2.IMREAD_COLOR)
+                        dst = cv2.resize(src, dsize=(width, height), interpolation=cv2.INTER_LINEAR)
+                        cv2.imwrite(savePath%cnt, dst)
+                        cnt = cnt + 1
+
+
+#%%
+#excute main
+if __name__ == '__main__':
+        load_path = 'C:/Users/zmfna/Documents/GitHub/SuicideSquad/data/raw/wait/'
+        save_path = "C:/Users/zmfna/Documents/GitHub/SuicideSquad/data/raw/wait/%d.jpg"
+        resize(load_path,save_path)
--- src/Image_Rotate.py
+++ src/Image_Rotate.py
@@ -1,12 +1,36 @@
+#%%
+#import modules
 import cv2
 import numpy as np
+import glob
+from os import listdir
+from os.path import isfile, join
+#%%
+#variable
+width = 112
+height = 112
 
-def rotate(imgPath):
-    src = cv2.imread(imgPath, cv2.IMREAD_COLOR)
-    height, width, channel = src.shape
-    rot = cv2.getRotationMatrix2D((width/2,height/2), 10,1)
-    dst = cv2.warpAffine(src,rot,(width,height))
-    cv2.imwrite(imgPath,dst)
+#%%
+#function
+def resize(loadPath,savePath):
+        images = listdir(loadPath)
 
-path = 'C:\\Users\\zmfna\\Desktop\\hand.jpg'
-rotate(path)
+        print(images)
+        for i in range(len(images)):
+                image = images[i]
+                cnt = 0
+                while image:
+                    src = cv2.imread(image, cv2.IMREAD_COLOR)
+                    height, width, channel = src.shape
+                    rot = cv2.getRotationMatrix2D((width/2,height/2), 10,1)
+                    dst = cv2.warpAffine(src,rot,(width,height))
+                    cv2.imwrite(savePath%cnt,dst)
+                    cnt = cnt + 1
+
+
+#%%
+#excute main
+if __name__ == '__main__':
+        load_path = 'C:/Users/zmfna/Documents/GitHub/SuicideSquad/data/raw/wait/'
+        save_path = "C:/Users/zmfna/Documents/GitHub/SuicideSquad/data/raw/wait/%d.jpg"
+        resize(load_path,save_path)
