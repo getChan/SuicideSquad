b323de2db583cc062a8c0d47bef5c466094171d9
getChan <9511chn@gmail.com>
2019-02-16 22:12:20 +0900

    Equalize Number of Frames

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,14 +4,20 @@ import os
 
 def VidToFrame(load_path):
     frames = list()
+    total_frames = FrameCount(load_path)
+    # 사용할 프레임 수
+    FRAME_NUM = 30
+    start_frame = np.random.randint(0, total_frames-FRAME_NUM)
+
     try:
         vidcap = cv2.VideoCapture(load_path)
         success, image = vidcap.read()
 
         if not success :
             print('Video Read Error')
-        cnt = 0
+        cnt = 0 
         while success:
+            
             success, image = vidcap.read()
 
             hsv = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
@@ -22,11 +28,15 @@ def VidToFrame(load_path):
             
             width = 1920
             height = 1080
+            
             mask = cv2.resize(mask, dsize=(width, height), interpolation=cv2.INTER_LINEAR)
 
             if cv2.waitKey(10) == 27:                     # exit if Escape is hit
                 break
-            frames.append(mask)
+            if cnt >= start_frame and cnt < start_frame+FRAME_NUM:
+                frames.append(mask)
+            cnt += 1
+
         vidcap.release()
     except Exception:
         pass
@@ -39,7 +49,7 @@ def FrameCount(load_path):
     return int(frameCount)
 
 def getDataFrame(load_path):
-    
+    ### 레이블 전처리
     labels=[]
     label_num = 0
     label_dict={'cute':0, 'hi':1, 'love':2, 'thank':3, 'wait':4}
@@ -58,6 +68,7 @@ def getDataFrame(load_path):
             # load_data
             for f in filename:
                 data.append(VidToFrame(parent+'\\'+f))
+                
     data = np.asarray(data)
     labels = np.asarray(labels,dtype=np.int)
 
