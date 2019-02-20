6f8bc52bbfad882940da1059d78089145777390c
hyunseok4475 <33482120+hyunseok4475@users.noreply.github.com>
2019-02-20 14:16:12 +0900

    이미지 그레이 스케일로 변환해서 합치기

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,42 +1,38 @@
+
 import cv2
 import numpy as np
 import os
 
 def VidToFrame(load_path):
     frames = list()
-    total_frames = FrameCount(load_path)
-    # 사용할 프레임 수
-    FRAME_NUM = 30
-    start_frame = np.random.randint(0, total_frames-FRAME_NUM)
-
     try:
         vidcap = cv2.VideoCapture(load_path)
         success, image = vidcap.read()
 
         if not success :
             print('Video Read Error')
-        cnt = 0 
+        cnt = 0
         while success:
-            
             success, image = vidcap.read()
 
             hsv = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
             lower_skin = np.array([0, 133, 77])
             upper_skin = np.array([255, 173, 127])
-
+            
             mask = cv2.inRange(hsv, lower_skin, upper_skin)
+            image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+            width = 720
+            height = 480
+            mask = cv2.resize(mask, dsize=(width, height), interpolation=cv2.INTER_AREA)
+            img = cv2.resize(image, dsize=(width, height), interpolation=cv2.INTER_AREA)
+
+            mask = mask - img
             
-            width = 1920
-            height = 1080
-            
-            mask = cv2.resize(mask, dsize=(width, height), interpolation=cv2.INTER_LINEAR)
 
             if cv2.waitKey(10) == 27:                     # exit if Escape is hit
                 break
-            if cnt >= start_frame and cnt < start_frame+FRAME_NUM:
-                frames.append(mask)
-            cnt += 1
-
+            frames.append(mask)
         vidcap.release()
     except Exception:
         pass
@@ -49,7 +45,7 @@ def FrameCount(load_path):
     return int(frameCount)
 
 def getDataFrame(load_path):
-    ### 레이블 전처리
+    
     labels=[]
     label_num = 0
     label_dict={'cute':0, 'hi':1, 'love':2, 'thank':3, 'wait':4}
@@ -68,7 +64,6 @@ def getDataFrame(load_path):
             # load_data
             for f in filename:
                 data.append(VidToFrame(parent+'\\'+f))
-                
     data = np.asarray(data)
     labels = np.asarray(labels,dtype=np.int)
 
