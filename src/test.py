c240443f12b02a67aee1c89fc8ab9aef78d96d94
getChan <201401438@inu.ac.kr>
2019-02-27 15:10:21 +0900

    use this code

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import preprocessing as pre
-from matplotlib import pyplot
-
-
-if __name__ == '__main__':
-    load_path = 'C:/Users/zmfna/Documents/GitHub/SuicideSquad/data/raw/cute/1.mp4'
-    frame = pre.VidToFrame(load_path)
-    print(frame)
-    
--- src/train.py
+++
@@ -1,7 +0,0 @@
-import os
-import time
-import numpy
-from six.moves import xrange
-import tensorflow as tf
-import input_data
-import model
