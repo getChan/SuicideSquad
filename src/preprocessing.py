b897d272f075120a8e82519af5b4d721f992f46b
getChan <201401438@inu.ac.kr>
2019-04-01 19:55:17 +0900

    Export Model

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -20,22 +20,22 @@ def VidToFrame(load_path):
             
             success, image = vidcap.read()
 
-            # hsv = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
-            # lower_skin = np.array([0, 133, 77])
-            # upper_skin = np.array([255, 173, 127])
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
+            lower_skin = np.array([0, 133, 77])
+            upper_skin = np.array([255, 173, 127])
 
-            # skinMask = cv2.inRange(hsv, lower_skin, upper_skin)
-            # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
-            # skinMask = cv2.erode(skinMask, kernel, iterations = 2)
-            # skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
-            # skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
-            # skin = cv2.bitwise_and(image, image, mask = skinMask)
+            skinMask = cv2.inRange(hsv, lower_skin, upper_skin)
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+            skinMask = cv2.erode(skinMask, kernel, iterations = 2)
+            skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
+            skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+            skin = cv2.bitwise_and(image, image, mask = skinMask)
 
             width = 112
             height = 112
             
-            # mask = cv2.resize(skin, dsize=(width, height), interpolation=cv2.INTER_LINEAR)
-            mask = cv2.resize(image, dsize=(width, height), interpolation=cv2.INTER_LINEAR)
+            mask = cv2.resize(skin, dsize=(width, height), interpolation=cv2.INTER_LINEAR)
+            # mask = cv2.resize(image, dsize=(width, height), interpolation=cv2.INTER_LINEAR)
 
             if cv2.waitKey(10) == 27:                     # exit if Escape is hit
                 break
